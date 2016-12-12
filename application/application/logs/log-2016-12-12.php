bb2b8380e287840a31edbb2bd699f68dc1185de2
Mickjr <mantonhorton@gmail.com>
2016-12-11 21:29:44 -0500

    Delete extra files

--- application/application/logs/log-2016-12-12.php
+++
@@ -1,3 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2016-12-12 01:42:49 --> Severity: error --> Exception: Call to undefined function base_url() C:\MAMP\htdocs\AJAX_CRUD\application\views\add_view.php 8
